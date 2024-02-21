c48462778e7fb07d98a0e2581131e7c41d15d3e8
Jay Shah <jayyshah69@gmail.com>
2024-02-21 08:36:20 -0500

    Create draft for the HTML and CSS. Setup the board and create an onclick function for it which updates board array

--- script.js
+++ script.js
@@ -3,16 +3,14 @@
  * Player:
  * Can play the game
  * Has a name
+ * Has it's symbol
  */
-const player = (given_name) => {
+const player = (given_name, playerSymbol) => {
 
     const name = given_name
+    const symbol = playerSymbol
 
-    const play = (row, col, symbol) => {
-        gameBoard.play(row, col, symbol)
-    }
-
-    return {name, play}
+    return {name, symbol}
 }
 
 /**
@@ -21,14 +19,17 @@ const player = (given_name) => {
  * Can print itself on a the DOM
  * Can reset itself back to it's initial state
  * Can change state for one of it's cell, one at a time.
+ * Can Check if there is a winner
  */
 const gameBoard = (function () {
     board = [];
 
-    for(i = 0; i < 3; i ++){
-        board[i] = [];
-        for(j = 0; j < 3; j++) {
-            board[i][j] = i*3 + j;
+    const clearBoard = () => {
+        for(i = 0; i < 3; i ++){
+            board[i] = [];
+            for(j = 0; j < 3; j++) {
+                board[i][j] = '';
+            }
         }
     }
 
@@ -40,11 +41,85 @@ const gameBoard = (function () {
         }
     }
 
-    const play = (row, col, symbol) => {
-        board[row][col] = symbol
+    const play = (event) => {
+        board[event.currentTarget.dataset.row][event.currentTarget.dataset.col] = activePlayerSymbol
+        flow.changeActivePlayer()
+        gameBoard.toString()
+        if(gameBoard.checkWinner(player1.symbol)) console.log('p1wins')
+        else if(gameBoard.checkWinner(player2.symbol)) console.log('p2wins')
+
     }
 
-    return {toString}
+    const setUpBoard = () => {
+        gameBoardElem = document.createElement('div')
+        container = document.querySelector('.container')
+
+        gameBoardElem.className = 'board'
+
+        setPeices(gameBoardElem)
+
+        container.appendChild(gameBoardElem)
+        
+    }
+
+    const setPeices = (gameBoardElem) => {
+        for(i = 0; i < board.length; i++){
+            for(j = 0; j < board[i].length; j++){
+                
+                elem = document.createElement('div')
+                elem.className = 'boardItem'
+                elem.dataset.row= i
+                elem.dataset.col=j
+
+                elem.addEventListener('click', (event) => {
+                    gameBoard.play(event);
+                })
+
+                gameBoardElem.appendChild(elem)
+            }
+        }
+    }
+
+    const checkWinner = (symbol) => {
+        if(checkRow(symbol) || checkColumn(symbol) || checkDiag(symbol)) return true
+        else return false        
+    }
+
+    const checkRow = (symbol) => {
+        for(i = 0; i < board.length; i++){
+            win = true
+            for(j = 0; j < board[i].length; j++){
+                if(board[i][j] != symbol) win = false;
+            }
+            if(win == true) return true
+        }
+        return false
+    }
+
+    const checkColumn = (symbol) => {
+        for(i = 0; i < board[0].length; i++){
+            win = true
+            for(j = 0; j < board.length; j++){
+                if(board[j][i] != symbol) win = false;
+            }
+            if(win == true) return true;
+        }
+        return false
+    }
+
+    const checkDiag = (symbol) => {
+        diag1 = true
+        diag2 = true
+        for(i = 0; i < board.length; i++){
+            if(board[i][i] != symbol) diag1 = false
+            if(board[board.length - i - 1][board[i].length - i - 1] != symbol) diag2 = false
+        }
+
+        if(diag1 | diag2) return true
+        else return false
+    }
+
+    return {play, toString, checkWinner, setUpBoard, clearBoard}
 })()
 
 /**
@@ -52,11 +127,23 @@ const gameBoard = (function () {
  * Initiallizes two players
  */
 const flow = (function () {
-    p1_name = prompt('What is the name of player 1: ')
-    p2_name = prompt('What is the name of player 2: ')
-    player1 = player(p1_name)
-    player2 = player(p2_name)
+    const s1 = '*'
+    const s2 = 'O'
+    // p1_name = prompt('What is the name of player 1: ')
+    // p2_name = prompt('What is the name of player 2: ')
+    player1 = player('Turtle', s1)
+    player2 = player('Yertle', s2)
+    gameBoard.clearBoard()
+    activePlayerSymbol = '*'
 
-    
+    gameBoard.setUpBoard()
+
+    const changeActivePlayer = () => {
+        activePlayerSymbol == s2 ? 
+        activePlayerSymbol = player1.symbol :
+        activePlayerSymbol = player2.symbol
+    }
+
+    return {changeActivePlayer}
 })()
 
